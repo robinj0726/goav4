a5a449ed662b03b8cdb6d078b4e0d4663b4f507e
bji <robinj0726@hotmail.com>
2022-05-14 23:28:21 +0800

    prefer using go struct

--- avformat/avformat.go
+++ avformat/avformat.go
@@ -1,11 +1,26 @@
 package avformat
 
 /*
-#cgo pkg-config: libavformat
+#cgo pkg-config: libavformat libavcodec libavutil
 #include <libavformat/avformat.h>
 */
 import "C"
+import "unsafe"
 
-func AvformatVersion() uint {
+type AVFormatContext struct {
+	ctx *C.struct_AVFormatContext
+}
+
+func AllocContext() *AVFormatContext {
+	return &AVFormatContext{
+		ctx: (*C.struct_AVFormatContext)(C.avformat_alloc_context()),
+	}
+}
+
+func (f *AVFormatContext) FreeContext() {
+	C.avformat_free_context((*C.struct_AVFormatContext)(unsafe.Pointer(f.ctx)))
+}
+
+func Version() uint {
 	return uint(C.avformat_version())
 }
