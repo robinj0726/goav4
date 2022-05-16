c3b7fa4bac1f84d95700e4059ba443bcedff6632
bji <robinj0726@hotmail.com>
2022-05-16 21:42:13 +0800

    add avformat_close_input

--- avformat/avformat.go
+++ avformat/avformat.go
@@ -5,7 +5,11 @@ package avformat
 #include <libavformat/avformat.h>
 */
 import "C"
-import "unsafe"
+import (
+	"errors"
+	"fmt"
+	"unsafe"
+)
 
 type AVFormatContext struct {
 	ctx *C.struct_AVFormatContext
@@ -21,12 +25,24 @@ func (c *AVFormatContext) FreeContext() {
 	C.avformat_free_context((*C.struct_AVFormatContext)(unsafe.Pointer(c.ctx)))
 }
 
-func (c *AVFormatContext) OpenInput(url string) int {
-	return (int)(C.avformat_open_input((**C.struct_AVFormatContext)(&c.ctx), C.CString(url), (*C.struct_AVInputFormat)(unsafe.Pointer(uintptr(0))), (**C.struct_AVDictionary)(unsafe.Pointer(uintptr(0)))))
+func (c *AVFormatContext) OpenInput(url string) error {
+	ret := (int)(C.avformat_open_input((**C.struct_AVFormatContext)(&c.ctx), C.CString(url), (*C.struct_AVInputFormat)(unsafe.Pointer(uintptr(0))), (**C.struct_AVDictionary)(unsafe.Pointer(uintptr(0)))))
+	if ret < 0 {
+		return fmt.Errorf("Could not open source file %s", url)
+	}
+	return nil
 }
 
-func (c *AVFormatContext) FindStreamInfo() int {
-	return (int)(C.avformat_find_stream_info((*C.struct_AVFormatContext)(c.ctx), (**C.struct_AVDictionary)(unsafe.Pointer(uintptr(0)))))
+func (c *AVFormatContext) CloseInput() {
+	C.avformat_close_input((**C.struct_AVFormatContext)(&c.ctx))
+}
+
+func (c *AVFormatContext) FindStreamInfo() error {
+	ret := (int)(C.avformat_find_stream_info((*C.struct_AVFormatContext)(c.ctx), (**C.struct_AVDictionary)(unsafe.Pointer(uintptr(0)))))
+	if ret < 0 {
+		return errors.New("Could not find stream information")
+	}
+	return nil
 }
 
 func (c *AVFormatContext) DumpFormat() {
