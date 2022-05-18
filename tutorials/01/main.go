03c3cbdb49776df248199b6995a4b62cdac47083
bji <robinj0726@hotmail.com>
2022-05-18 09:55:16 +0800

    copy codec parameters to codec context

--- tutorials/01/main.go
+++ tutorials/01/main.go
@@ -10,19 +10,27 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a movie file")
+		return
+	}
+
 	fmt_ctx := avformat.AVFormatContext{}
 	fmt_ctx.OpenInput(os.Args[1])
 	defer fmt_ctx.CloseInput()
 
 	fmt_ctx.FindStreamInfo()
 
-	for codecpar := range fmt_ctx.AVStreams() {
-		if codecpar.CodecType == avutil.AVMEDIA_TYPE_VIDEO {
-			codec, _ := avcodec.FindDecoder(codecpar.CodecID)
-			avctx, _ := avcodec.AllocContext3(codec)
-			fmt.Printf("%#v\n", avctx)
-			avctx.Open2(codec)
+	var codec *avcodec.AVCodec
+	var avctx *avcodec.AVCodecContext
+	for stream := range fmt_ctx.AVStreams() {
+		if stream.CodecType() == avutil.AVMEDIA_TYPE_VIDEO {
+			codec, _ = avcodec.FindDecoder(stream.CodecID())
+			avctx, _ = avcodec.AllocContext3(codec)
+			avctx.ParametersToContext(stream.CodecParameters())
+			fmt.Println(avctx)
 		}
 	}
+	avctx.Open2(codec)
 
 }
