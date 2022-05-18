39070ec552ef507b783464e0efb1b427bbb317c6
bji <robinj0726@hotmail.com>
2022-05-19 07:11:44 +0800

    add util.c/h

--- tutorials/01/main.go
+++ tutorials/01/main.go
@@ -1,29 +1,7 @@
 package main
 
 /*
-#include <libavutil/frame.h>
-
-void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame) {
-  FILE *pFile;
-  char szFilename[32];
-  int  y;
-
-  // Open file
-  sprintf(szFilename, "frame%d.ppm", iFrame);
-  pFile=fopen(szFilename, "wb");
-  if(pFile==NULL)
-    return;
-
-  // Write header
-  fprintf(pFile, "P6\n%d %d\n255\n", width, height);
-
-  // Write pixel data
-  for(y=0; y<height; y++)
-    fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
-
-  // Close file
-  fclose(pFile);
-}
+#include "util.h"
 */
 import "C"
 import (
---
+++ tutorials/01/util.h
@@ -0,0 +1,3 @@
+#include <libavutil/frame.h>
+
+void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame);
