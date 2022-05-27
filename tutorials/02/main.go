ea2a806d40c11e0a58439b546cf6cd74d92c1ebb
bji <robinj0726@hotmail.com>
2022-05-27 23:19:34 +0800

    not done yet

--- tutorials/02/main.go
+++ tutorials/02/main.go
@@ -1,7 +1,10 @@
 package main
 
 /*
+#cgo pkg-config: libavutil
+
 #include <libavutil/frame.h>
+#include <SDL2/SDL_rect.h>
 
 static void SaveFrameToYUV(AVFrame *pFrame, int w, int h, int iFrame) {
   FILE *pFile;
@@ -24,22 +27,48 @@ static void SaveFrameToYUV(AVFrame *pFrame, int w, int h, int iFrame) {
   // Close file
   fclose(pFile);
 }
+
+static void calculate_display_rect(SDL_Rect *rect,
+                                   int scr_xleft, int scr_ytop, int scr_width, int scr_height,
+                                   int pic_width, int pic_height)
+{
+    AVRational aspect_ratio = {1,1};
+    int64_t width, height, x, y;
+
+    if (av_cmp_q(aspect_ratio, av_make_q(0, 1)) <= 0)
+        aspect_ratio = av_make_q(1, 1);
+
+    aspect_ratio = av_mul_q(aspect_ratio, av_make_q(pic_width, pic_height));
+
+    height = scr_height;
+    width = av_rescale(height, aspect_ratio.num, aspect_ratio.den) & ~1;
+    if (width > scr_width) {
+        width = scr_width;
+        height = av_rescale(width, aspect_ratio.den, aspect_ratio.num) & ~1;
+    }
+    x = (scr_width - width) / 2;
+    y = (scr_height - height) / 2;
+    rect->x = scr_xleft + x;
+    rect->y = scr_ytop  + y;
+    rect->w = FFMAX((int)width,  1);
+    rect->h = FFMAX((int)height, 1);
+}
+
 */
 import "C"
 import (
-	"time"
+	"unsafe"
 
 	"github.com/robinj730/goav4/avcodec"
 	"github.com/robinj730/goav4/avformat"
 	"github.com/robinj730/goav4/avutil"
 	"github.com/robinj730/goav4/sdl2"
-	"github.com/robinj730/goav4/swscale"
 )
 
 func main() {
 	infile := "../sample.mp4"
 
-	err := sdl2.Init(sdl2.INIT_VIDEO)
+	err := sdl2.Init(sdl2.INIT_VIDEO | sdl2.INIT_TIMER)
 	if err != nil {
 		panic(err)
 	}
@@ -76,16 +105,11 @@ func main() {
 		panic(err)
 	}
 
-	pFrame := avutil.FrameAlloc()
-	defer pFrame.Free()
-
-	pFrameYUV := avutil.FrameAlloc()
-	defer pFrameYUV.Free()
-
-	avutil.ImageAlloc(pFrameYUV, pCodecCtx.Width(), pCodecCtx.Height(), pCodecCtx.PixFmt(), 16)
+	// pFrameYUV := avutil.FrameAlloc()
+	// defer pFrameYUV.Free()
 
 	// fmt.Println(pCodecCtx)
-	sws_ctx, _ := swscale.GetContext(pCodecCtx.Width(), pCodecCtx.Height(), pCodecCtx.PixFmt(), pCodecCtx.Width(), pCodecCtx.Height(), int(avutil.AV_PIX_FMT_YUV420P), swscale.SWS_BILINEAR)
+	// sws_ctx, _ := swscale.GetContext(pCodecCtx.Width(), pCodecCtx.Height(), pCodecCtx.PixFmt(), pCodecCtx.Width(), pCodecCtx.Height(), int(avutil.AV_PIX_FMT_YUV420P), swscale.SWS_BILINEAR)
 
 	pkt := avcodec.PacketAlloc()
 	defer pkt.Free()
@@ -97,20 +121,24 @@ func main() {
 	}
 	defer window.Destroy()
 
-	render, err := sdl2.CreateRenderer(window, -1, 0)
+	render, err := sdl2.CreateRenderer(window, -1, sdl2.RENDERER_ACCELERATED|sdl2.RENDERER_PRESENTVSYNC)
 	if err != nil {
 		panic(err)
 	}
 	defer render.Destroy()
 
-	texture, err := render.CreateTexture(sdl2.PIXELFORMAT_YV12, sdl2.TEXTUREACCESS_STREAMING, pCodecCtx.Width(), pCodecCtx.Height())
+	texture, err := render.CreateTexture(sdl2.PIXELFORMAT_IYUV, sdl2.TEXTUREACCESS_STREAMING, pCodecCtx.Width(), pCodecCtx.Height())
 	if err != nil {
 		panic(err)
 	}
 	defer texture.Destroy()
+	texture.SetBlendMode(sdl2.BLENDMODE_NONE)
 
 	n := 0
 	for {
+		pFrame := avutil.FrameAlloc()
+		defer pFrame.Free()
+
 		err := pFormatCtx.ReadFrame(pkt.PacketPtr())
 		if err != nil {
 			break
@@ -127,25 +155,36 @@ func main() {
 				panic(err)
 			}
 
-			// C.SaveFrameToYUV((*C.struct_AVFrame)(pFrame.FrameRef()), (C.int)(pCodecCtx.Width()), (C.int)(pCodecCtx.Height()), (C.int)(n))
+			// pFrameYUV := avutil.FrameAlloc()
+			// avutil.ImageAlloc(pFrameYUV, pCodecCtx.Width(), pCodecCtx.Height(), pCodecCtx.PixFmt(), 16)
 
-			sws_ctx.Scale(pFrame, 0, pCodecCtx.Height(), pFrameYUV)
+			// sws_ctx.Scale(pFrame, 0, pCodecCtx.Height(), pFrameYUV)
 
-			texture.UpdateYUV(nil, (*byte)(pFrame.DataPtr(0)), pFrame.LineSize(0), (*byte)(pFrame.DataPtr(1)), pFrame.LineSize(0), (*byte)(pFrame.DataPtr(2)), pFrame.LineSize(2))
-
+			render.SetDrawColor(0, 0, 0, 255)
 			err = render.Clear()
 			if err != nil {
 				panic(err)
 			}
 
-			err = render.Copy(texture, nil, nil)
+			// C.SaveFrameToYUV((*C.struct_AVFrame)(pFrame.FramePtr()),
+			// 	(C.int)(pCodecCtx.Width()), (C.int)(pCodecCtx.Height()), (C.int)(n))
+
+			texture.UpdateYUV(nil, (*byte)(pFrame.DataPtr(0)), pFrame.LineSize(0),
+				(*byte)(pFrame.DataPtr(1)), pFrame.LineSize(1),
+				(*byte)(pFrame.DataPtr(2)), pFrame.LineSize(2))
+
+			rect := sdl2.Rect{}
+			C.calculate_display_rect((*C.SDL_Rect)(unsafe.Pointer(&rect)), 0, 0, C.int(pCodecCtx.Width()), C.int(pCodecCtx.Height()),
+				C.int(pCodecCtx.Width()), C.int(pCodecCtx.Height()))
+
+			err = render.CopyEx(texture, nil, nil, 0, nil, sdl2.FLIP_NONE)
 			if err != nil {
 				panic(err)
 			}
 
 			render.Present()
 
-			time.Sleep(30 * time.Millisecond)
+			sdl2.Delay(60)
 			n += 1
 		}
 	}
